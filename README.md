b584574e12fa6359ea50b821196593c9362de58f
Tugdual Kerjan <tkerjan@outlook.com>
2023-01-08 17:14:40 +0100

    Refactor

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# StartHack, St.Gallen, Switzerland
+# STARTHack2019
+
 Mubbling Voice Recognition via Machine learning through Microsoft Azure.
+
+Linked here is our submission to the contest : https://devpost.com/software/voice-clarifier
+
+This could for example be used in better voice enhancement after data loss / compression.
---
+++ main.py
@@ -0,0 +1,172 @@
+import requests, json
+
+import pyaudio
+import wave
+import os, requests, time, json, wave, pyaudio
+from xml.etree import ElementTree
+
+class Recorder(object):
+    '''A recorder class for recording audio to a WAV file.
+    Records in mono by default.
+    '''
+
+    def __init__(self, channels=1, rate=44100, frames_per_buffer=1024):
+        self.channels = channels
+        self.rate = rate
+        self.frames_per_buffer = frames_per_buffer
+
+    def open(self, fname, mode='wb'):
+        return RecordingFile(fname, mode, self.channels, self.rate,
+                            self.frames_per_buffer)
+
+class RecordingFile(object):
+    def __init__(self, fname, mode, channels, 
+                rate, frames_per_buffer):
+        self.fname = fname
+        self.mode = mode
+        self.channels = channels
+        self.rate = rate
+        self.frames_per_buffer = frames_per_buffer
+        self._pa = pyaudio.PyAudio()
+        self.wavefile = self._prepare_file(self.fname, self.mode)
+        self._stream = None
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exception, value, traceback):
+        self.close()
+
+    def record(self, duration):
+        # Use a stream with no callback function in blocking mode
+        self._stream = self._pa.open(format=pyaudio.paInt16,
+                                        channels=self.channels,
+                                        rate=self.rate,
+                                        input=True,
+                                        frames_per_buffer=self.frames_per_buffer)
+        for _ in range(int(self.rate / self.frames_per_buffer * duration)):
+            audio = self._stream.read(self.frames_per_buffer)
+            self.wavefile.writeframes(audio)
+        return None
+
+    def start_recording(self):
+        # Use a stream with a callback in non-blocking mode
+        self._stream = self._pa.open(format=pyaudio.paInt16,
+                                        channels=self.channels,
+                                        rate=self.rate,
+                                        input=True,
+                                        frames_per_buffer=self.frames_per_buffer,
+                                        stream_callback=self.get_callback())
+        self._stream.start_stream()
+        return self
+
+    def stop_recording(self):
+        self._stream.stop_stream()
+        return self
+
+    def get_callback(self):
+        def callback(in_data, frame_count, time_info, status):
+            self.wavefile.writeframes(in_data)
+            return in_data, pyaudio.paContinue
+        return callback
+
+
+    def close(self):
+        self._stream.close()
+        self._pa.terminate()
+        self.wavefile.close()
+
+    def _prepare_file(self, fname, mode='wb'):
+        wavefile = wave.open(fname, mode)
+        wavefile.setnchannels(self.channels)
+        wavefile.setsampwidth(self._pa.get_sample_size(pyaudio.paInt16))
+        wavefile.setframerate(self.rate)
+        return wavefile
+
+rec = Recorder(channels=2)
+with rec.open('blocker.wav', 'wb') as recfile:
+     recfile.record(duration=5.0)
+
+
+headers = {'Ocp-Apim-Subscription-Key': '6149675feef147f797f24802e78aafac'}
+url = 'https://francecentral.stt.speech.microsoft.com/speech/recognition/dictation/cognitiveservices/v1?cid=48b1fa24-f21f-4b41-8c3f-be319d8b4b26'
+r = requests.post(url, headers=headers, data=open('blocker.wav', 'rb').read())
+resp = json.loads(r.text)
+print(resp["DisplayText"])
+
+##Playback
+subscription_key = "28024669eb534248bcf6e42329a07558"
+
+if 'SPEECH_SERVICE_KEY' in os.environ:
+    subscription_key = os.environ['SPEECH_SERVICE_KEY']
+elif subscription_key == None:
+    print('Environment variable for your subscription key is not set.')
+    exit()
+
+class TextToSpeech(object):
+    def __init__(self, subscription_key):
+        self.subscription_key = subscription_key
+        self.timestr = time.strftime("%Y%m%d-%H%M")
+        self.access_token = None
+
+    def get_token(self):
+        fetch_token_url = "https://westus.api.cognitive.microsoft.com/sts/v1.0/issueToken"
+        headers = {
+            'Ocp-Apim-Subscription-Key': self.subscription_key
+        }
+        response = requests.post(fetch_token_url, headers=headers)
+        print(response.text)
+        self.access_token = str(response.text)
+
+    def save_audio(self):
+        base_url = 'https://westus.tts.speech.microsoft.com/'
+        path = 'cognitiveservices/v1'
+        constructed_url = base_url + path
+        headers = {
+            'Authorization': 'Bearer ' + self.access_token,
+            'Content-Type': 'application/ssml+xml',
+            'X-Microsoft-OutputFormat': 'riff-24khz-16bit-mono-pcm',
+            'User-Agent': 'STARTHack'
+        }
+        xml_body = ElementTree.Element('speak', version='1.0')
+        xml_body.set('{http://www.w3.org/XML/1998/namespace}lang', 'en-us')
+        voice = ElementTree.SubElement(xml_body, 'voice')
+        voice.set('{http://www.w3.org/XML/1998/namespace}lang', 'ar-EG')
+        voice.set('name', 'Microsoft Server Speech Text to Speech Voice (ar-EG, Hoda)')
+        voice.text = resp["DisplayText"]
+        body = ElementTree.tostring(xml_body)
+
+        response = requests.post(constructed_url, headers=headers, data=body)
+        '''
+        If a success response is returned, then the binary audio is written
+        to file in your working directory. It is prefaced by sample and
+        includes the date.
+        '''
+        if response.status_code == 200:
+            with open('gogogo.wav', 'wb') as audio:
+                audio.write(response.content)
+                print("\nStatus code: " + str(response.status_code) + "\nYour TTS is ready for playback.\n")
+
+            chunk = 1024
+            f = wave.open(r"gogogo.wav","rb")
+            p = pyaudio.PyAudio()
+            stream = p.open(format = p.get_format_from_width(f.getsampwidth()),  
+            channels = f.getnchannels(),  
+            rate = f.getframerate(),  
+            output = True)
+            data = f.readframes(chunk) 
+            while data:  
+                stream.write(data)  
+                data = f.readframes(chunk)  
+
+            stream.stop_stream()  
+            stream.close()   
+            p.terminate() 
+        else:
+            print("\nStatus code: " + str(response.status_code) + "\nSomething went wrong. Check your subscription key and headers.\n")
+            print(response.headers)
+
+if __name__ == "__main__":
+    app = TextToSpeech(subscription_key)
+    app.get_token()
+    app.save_audio()
